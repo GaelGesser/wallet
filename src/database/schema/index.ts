7b9ac69562a5922c2ea834f72c99a7fca15e2a62
Gabriel Gesser <gabrielhenrig@outlook.com>
2025-10-14 21:32:05 -0300

    refactor: streamline accounts schema definition and enhance schema export structure

--- src/database/schema/index.ts
+++ src/database/schema/index.ts
@@ -1,1 +1,11 @@
-export const schema = {};
+import { accounts } from './accounts';
+import { sessions } from './sessions';
+import { users } from './users';
+import { verifications } from './verifications';
+
+export const schema = {
+  accounts,
+  sessions,
+  users,
+  verifications,
+};
